ee28b3b796edd1f1dc1d37533cc1e9857e6677ac
hyaghogomes12-maker <hyaghogomes12@gmail.com>
2026-09-21 17:34:02 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-const CACHE = 'cozinha-connect-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => { const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return resp; }).catch(() => caches.match('./index.html'))));
+const CACHE_NAME="cozinha-connect-v2";
+const APP_SHELL=["./","./index.html","./manifesto.webmanifest","./icone-192.png","./icone-512.png","./icone-512-maskable.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./"))));
 });
